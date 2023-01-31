2097bfde5b685a75cd12035ffe95d99e8ac3d02e
qwertykg <keerangovender1@gmail.com>
2023-01-31 23:19:28 +0200

    initial commit

---
+++ starter/build/precache-manifest.8a381aeefa93a37aae312cda2f9297cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb071420a60210889a82d85f06812734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4167e53da6cf89104369",
+    "url": "/static/css/main.d1c2b6d7.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4167e53da6cf89104369",
+    "url": "/static/js/main.d32c4f2c.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  },
+  {
+    "revision": "aea59904b3809f90c01a1bea235abd05",
+    "url": "/static/media/bg.aea59904.png"
+  }
+]);
